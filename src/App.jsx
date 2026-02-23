30c7798b69491f90a90ca409a6813add12d2cd12
LAmena-dev <amena2laremm@gmail.com>
2026-02-23 13:24:35 +0800

    1.0 - Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import { createBrowserRouter, RouterProvider } from "react-router";
+// import routes from "./routes.jsx";
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
